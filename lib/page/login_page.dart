dd000fd59f39e4706486f4fdf24877eec3245ad5
Cedric Hommann <cedric.hommann@gmx.de>
2023-09-15 15:57:54 +0200

    LoginPage visuals updated

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gasta_core/gasta_core.dart';
+import 'package:gasta_core/gasta_core.dart' as core;
 
 import '../controller/login_page_controller.dart';
 
@@ -19,29 +20,97 @@ class _LoginPage extends State<LoginPage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(children: [
-            Row(children: [
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
               Text(
                 "Login",
                 style: TextStyles.bigHeadlineTextStyle(context),
               ),
-            ]),
-            Expanded(
-              child: Container(
-                alignment: Alignment.center,
-                child: widget.controller.isLoading.value
-                    ? const CircularProgressIndicator()
-                    : DefaultButton(
-                        onPressed: () {
-                          setState(() {
-                            widget.controller.loginAsync();
-                          });
-                        },
-                        child: const Text("Login"),
-                      ),
-              ),
-            )
-          ]),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.center,
+                  child: widget.controller.isLoading.value
+                      ? const CircularProgressIndicator()
+                      : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const SizedBox(
+                              width: 300,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'Username',
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(40)),
+                                    borderSide: BorderSide(
+                                        color: Colors.greenAccent, width: 2.0),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(40)),
+                                    borderSide: BorderSide(
+                                        color: Colors.greenAccent, width: 2.0),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const SizedBox(
+                              width: 300,
+                              child: TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  hintText: 'Password',
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(40)),
+                                    borderSide: BorderSide(
+                                        color: Colors.greenAccent, width: 2.0),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(40)),
+                                    borderSide: BorderSide(
+                                        color: Colors.greenAccent, width: 2.0),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                core.OutlinedButton(
+                                  onPressed: () {
+                                    setState(() {});
+                                  },
+                                  child: const Text("Sign Up"),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                DefaultButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      widget.controller.loginAsync();
+                                    });
+                                  },
+                                  child: const Text("Login"),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
